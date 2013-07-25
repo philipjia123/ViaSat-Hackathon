088ad8ff58db0578d6f1d6abf9e5d27af287b4b5
Philip Jia <philipjia123@gmail.com>
2013-07-25 05:29:08 -0700

    Create video.js.watermark.js
    New working version thanks to Kalvin

---
+++ video.js.watermark.js
@@ -0,0 +1,128 @@
+console.log('watermark: Start');
+
+(function() {
+  console.log('watermark: Init defaults');
+  var defaults = {
+        file: 'square.png',
+        topLeft: [0, 0],
+        bottomRight: [0, 0],
+        xrepeat: 0,
+  	link: "about:blank",
+		tooltip: "None",
+        opacity: 0.0,
+		index : 0
+    },
+    extend = function() {
+      var args, target, i, object, property;
+      args = Array.prototype.slice.call(arguments);
+      target = args.shift() || {};
+      for (i in args) {
+        object = args[i];
+        for (property in object) {
+          if (object.hasOwnProperty(property)) {
+            if (typeof object[property] === 'object') {
+              target[property] = extend(target[property], object[property]);
+            } else {
+              target[property] = object[property];
+            }
+          }
+        }
+      }
+      return target;
+    };
+
+  /**
+   * register the thubmnails plugin
+   */
+  videojs.plugin('watermark', function(options) {
+    console.log('watermark: Register init');
+
+    var settings, video, div, img, player;
+    settings = extend(defaults, options);
+
+    /* Grab the necessary DOM elements */
+    video = this.el();
+
+	//Grab video player
+	player = window.my_video_id;
+	console.debug('player', player);
+
+    // create the watermark element
+    div = document.createElement('div');
+	div.setAttribute("id",options.index);
+    img = document.createElement('img');
+    div.appendChild(img);
+
+	//Video resize function
+	/*var resize = function(){
+		console.log('fullscreen change!');
+	}
+	
+	//Video fullscreen event
+	player.on("fullscreenchange", console.log("hi"));*/
+
+	//Click event
+	img.onclick = function(){
+		window.open(options.link);
+		player.pause();
+	}
+
+	//Mouse over event
+	img.onmouseover = function(){
+		div.style.opacity = options.opacity + 0.3;
+	}
+
+	//Mouse out event
+	img.onmouseout = function(){
+		div.style.opacity = options.opacity;
+	}
+
+	//Mouse down event
+	img.onmousedown = function(){
+		div.style.opacity = options.opacity + 0.5;
+	}
+
+	//Remove function
+	var remove = function(){
+		video.removeChild(div);
+	}
+
+	//Show function
+	var show = function(){
+		options.opacity = 0.25;
+		div.style.opacity = options.opacity;
+	}
+
+	//Hide function
+	var hide = function(){
+		options.opacity = 0;
+		div.style.opacity = options.opacity;
+	}
+
+    img.className = 'vjs-watermark';
+
+	//Grab image file
+    img.src = options.file;
+
+	var top = String(options.topLeft[1] * player.height() + "px");
+	var left = String(options.topLeft[0] * player.width() + "px");
+
+	var h = String((options.bottomRight[1] - options.topLeft[1])  * player.height() + "px");
+	var w = String((options.bottomRight[0] - options.topLeft[0]) * player.width() + "px");
+
+	img.style.top = top;
+	img.style.left = left;
+	img.style.height = h;
+	img.style.width = w;
+	
+    div.style.opacity = 0.25;
+
+
+    // add the watermark to the player
+    video.appendChild(div);
+
+    //video.oncontextmenu = function(){alert("Hello!");}
+    console.log('watermark: Register end');
+	return this;
+  });
+})();
